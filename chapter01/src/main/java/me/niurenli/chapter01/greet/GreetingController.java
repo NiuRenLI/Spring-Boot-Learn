a7db9e57817b197a243c4e534cf0a7148ecb25a5
niurenli <1353602729@Qq.com>
2018-09-20 15:28:24 +0800

    RESTful Web Service Controller

--- chapter01/src/main/java/me/niurenli/chapter01/greet/GreetingController.java
+++ chapter01/src/main/java/me/niurenli/chapter01/greet/GreetingController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * RESTful Web Service Controller
+ * 和普通MVC控制器最大区别是：返回不同。前者返回资源类对象，后者返回视图
+ */
 @RestController
 public class GreetingController {
     private static final String template = "Hello,%s!";
-    private final AtomicLong counter = new AtomicLong();
+
+    private final AtomicLong counter = new AtomicLong();//线程安全
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name) {
